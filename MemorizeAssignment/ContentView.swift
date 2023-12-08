1eb218a621aa38c38289e37e958baaa06a90f1a8
dkavla <dkowalczyk135@gmail.com>
2023-12-07 21:07:19 -0500

    Create CardView for creating individual cards Provide demo HStack for storing cards

--- MemorizeAssignment/ContentView.swift
+++ MemorizeAssignment/ContentView.swift
@@ -14,13 +14,30 @@ struct ContentView: View {
     @State var cardCount: Int = 1 // keeps track of the card count
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(content: emojis[0])
+            CardView(content: emojis[1])
+            CardView(content: emojis[2])
+            CardView(content: emojis[3])
         }
         .padding()
+    }
+}
+
+
+struct CardView: View {
+    var content: String
+    var isFaceUp = true
+    
+    @State var base = RoundedRectangle(cornerRadius: 12)
+    var body: some View {
+        ZStack {
+            Text(content)
+                .font(.largeTitle)
+            base.stroke(lineWidth: 2)
+        }
+        .foregroundColor(.orange)
+        
     }
 }
 
