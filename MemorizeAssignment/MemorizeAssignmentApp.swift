fd79ea72ae0de62210ebc5b8cfdead10376d2dfb
dkavla <dkowalczyk135@gmail.com>
2023-12-07 20:39:29 -0500

    Initial Commit

---
+++ MemorizeAssignment/MemorizeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAssignmentApp.swift
+//  MemorizeAssignment
+//
+//  Created by DK on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
