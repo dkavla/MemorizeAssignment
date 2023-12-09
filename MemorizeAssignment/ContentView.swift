614867b27394b9cacaa874c5bebdeeed2b0e9cb0
dkavla <dkowalczyk135@gmail.com>
2023-12-09 00:08:29 -0500

    Provide cardAdjuster function to create the individual adjuster buttons Add a title to the top of the screen Make the card display a LazyVGrid thats adaptive Place the contents of CardView in a Group within a ZStack Set opacity property to  Group to make contents transparent or base opaque based on isFaceUp value

--- MemorizeAssignment/ContentView.swift
+++ MemorizeAssignment/ContentView.swift
@@ -14,50 +14,76 @@ struct ContentView: View {
     @State var cardCount: Int = 4 // keeps track of the card count
     
     var body: some View {
-        HStack {
-            ForEach(0..<cardCount, id: \.self) { index in
-                CardView(content: emojis[index])
-            }
-        }
-        HStack {
-            Button(action: {
-                if cardCount > 1 {
-                    cardCount -= 1
-                }
-            }, label: {
-                Image(systemName: "minus.circle")
-            })
-            .font(.largeTitle)
+        VStack {
+            Text("Memorize").font(.largeTitle).foregroundColor(.blue).fontWeight(.bold)
+            card
             Spacer()
-            Button(action: {
-                if cardCount < emojis.count {
-                    cardCount += 1
-                }
-            }, label: {
-                Image(systemName: "plus.circle")
-            })
-            .font(.largeTitle)
+            cardCountAdjusters
         }
         .padding()
     }
     
+    // function that creates a Button with an action and symbol image
+    func cardAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    // displays the current amount of cards on screen
+    var card: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    // represnts the lower part of the display with buttons
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .font(.largeTitle)
+        .imageScale(.large)
+    }
+    
+    // variable that represents the Button for
+    // removing cards
+    var cardRemover: some View {
+        cardAdjuster(by: -1, symbol: "minus.circle")
+    }
+    
+    // variable that represents the Button for
+    // adding cards
+    var cardAdder: some View {
+        cardAdjuster(by: +1, symbol: "plus.circle")
+    }
 }
 
-
+// creates the card with emoji inside it
 struct CardView: View {
     var content: String
     @State var isFaceUp = true
     
-    var base = RoundedRectangle(cornerRadius: 12)
     var body: some View {
         ZStack {
-            if isFaceUp {
+            var base = RoundedRectangle(cornerRadius: 12)
+            Group {
                 base.fill(.white)
-                Text(content)
+                Text(content).font(.largeTitle)
                 base.stroke(lineWidth: 2).foregroundColor(.orange)
-            } else {
-                base.fill(.orange)
             }
+            // opacity is used to make the Group contents fill if face down
+            // and opacity is used to make the fill color transparent if face up
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill(.orange).opacity(isFaceUp ? 0 : 1)
         }.onTapGesture {
             isFaceUp.toggle()
         }
